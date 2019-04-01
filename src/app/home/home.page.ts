2af44233557b284ae892496ccfbb2c158ee8e907
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-03-31 23:52:38 -0300

    Implementação de modelo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Aluno } from '../escola/Aluno';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public alunos: Aluno[];
+  
+  ngOnInit(): void {
+    this.alunos = [
+      {
+        nome: "José",
+        endereco: "Rua Sei Lá"
+      },
+      {
+        nome: "Marcos",
+        endereco:"Rua Não Sei" 
+      },
+      
+
+    ]
+  }
 
 }
